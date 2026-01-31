73e81fc60b5c29c4ae613ad959f2077d69803473
Zakharko Adrian <zakharkoadr@gmail.com>
2026-01-31 22:35:17 +0200

    Variables and data types

---
+++ CSharplearning/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CSharplearning;
+
+namespace CSharplearning
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Тема 1: Variables and data types");
+            new VariablesAndTypes();
+        }
+    }
+};
---
+++ CSharplearning/VariablesAndTypes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace CSharplearning
+{
+    internal class VariablesAndTypes
+    {
+        public VariablesAndTypes()
+        {
+            //змінні та типи даних
+
+            int number = 10;
+            Console.WriteLine(number);
+            number = 20;
+            Console.WriteLine("Result: " + number);
+
+
+            byte byte1 = 255; // від 0 до 255
+            short short1 = 32767; // від -32 768 до 32 767
+            ushort ushort1 = 1; // 	від 0 до 65 535
+            int int1 = 0;
+            uint only_positive = 15; // тільки додатні там де "u"
+            long long1 = 0;
+            ulong ulong1 = 1;
+
+            // числа з крапкою
+            float float1 = 1.56765f;
+            double double1 = 1.923746012f;
+
+            // text
+            string string1 = "Hello";
+            char char1 = 'A';
+
+            // булеани
+            bool istrue = true;
+
+
+            int user1, user2;
+
+            user1 = Convert.ToInt32(Console.ReadLine());
+            user2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Result: " + user1 + "; Result: " + user2);
+        }    
+    }
+
+}
