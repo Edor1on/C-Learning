70dcad5ffabf54c8fbd7d9db00ad52c940827280
Zakharko Adrian <zakharkoadr@gmail.com>
2026-02-03 19:09:44 +0200

    Switch-case

---
+++ CSharplearning/Switch-case.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharplearning
+{
+    internal class Switch_case
+    {
+        public Switch_case()
+        {
+            short user_input = Convert.ToInt16(Console.ReadLine());
+
+            switch (user_input)
+            {
+                case 1:
+                    Console.WriteLine("Is 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Is 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Is 3");
+                    break;
+                case 4:
+                    Console.WriteLine("Is 4");
+                    break;
+                case 5:
+                    Console.WriteLine("Is 5");
+                    break;
+                default:
+                    Console.WriteLine("Is another number");
+                    break;
+
+            }
+        }
+    }
+}
